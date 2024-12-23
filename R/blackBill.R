984ffb93f93de2e16fada81feb5414e149c0509f
reshapedata <29032529@qq.com>
2024-12-23 09:15:19 +0000

    v1.0.2

--- R/blackBill.R
+++ R/blackBill.R
@@ -9,7 +9,7 @@
 #' blackBill_view()
 blackBill_view <- function(token) {
   sql=paste0('select FProjectNumber as 项目代码,FProjectName as 项目名称,FDate as 日期  from rds_t_rule_blackBill')
-  res=tsda::sql_select2(token = token,sql = sql)
+  res=tsda::sql_select2(token = token,sql =sql )
   return(res)
 
 }
